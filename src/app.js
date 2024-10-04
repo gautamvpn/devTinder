083094200171637e4287fbf7c806a433258dee1c
Vipin gautam <gautamvpn@gmail.com>
2024-10-04 19:49:19 +0530

    Exploring GET,POST,DELETE

--- src/app.js
+++ src/app.js
@@ -3,18 +3,31 @@ const express = require('express');
 // creating an instance of an express application
 const app = express();
 
-app.get("/", (req, res) => {
-    // send response from the server
-    res.send("Hello from the root route");
-});
 
-app.get("/hello", (req, res) => {
-    res.send("Hello Hello Hello....");
-});
+// this will handle GET call to /user
+app.get("/user",(req,res)=>{
+    res.send({firstName:"vipin",lastName:"gautam"})
+})
 
-app.get("/test", (req, res) => {
-    res.send("Hello from the server");
-});
+
+// for POST call only...
+app.post("/user",(req,res)=>{
+    // saving data to DB...
+    res.send("successfully adding new data.....")
+})
+
+
+// for delete call only...
+app.delete("/user",(req,res)=>{
+    res.send("deleting the current data...")
+})
+
+
+// this will match all the HTTP method API calls to /tests
+app.use("/test",(req,res)=>{
+    res.send('Hello from the server')
+})
+
 
 // listening on port 7777
 app.listen(7777, () => {
--- src/homework.md
+++ src/homework.md
@@ -8,3 +8,12 @@
 - What are dependencies
 - What is the use of "-g" while npm install
 - Difference between caret and tilde (^ vs ~)
+
+- Initialize git
+- .gitignore
+- create a remote repo on github
+- push all code to remote origin
+- play with routes and route extensions .hello, .hello/2, /xyz
+- Order of the routes matter a lot
+- Install Postman app and make a workspace/collection > test API call 
+- Write logic to handle GET,POST,PATCH,DELETE API Calls and test them on Postman
